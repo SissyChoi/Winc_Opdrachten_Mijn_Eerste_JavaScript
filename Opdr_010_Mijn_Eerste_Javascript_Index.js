870115eed169d8074ed4e72bca0eb02a66bc91ee
SissyChoi <90140566+SissyChoi@users.noreply.github.com>
2021-10-21 15:06:03 +0200

    Create Opdr_010_Mijn_Eerste_Javascript_Index.js

---
+++ Opdr_010_Mijn_Eerste_Javascript_Index.js
@@ -0,0 +1,35 @@
+console.log('Hello Winc Academy');
+
+let myName = 'Sissy Choi';
+console.log(myName);
+
+// antwoord vraag 6: nee kan niet, want het is een niet-semantische beschrijving van een keyword. 
+
+let sum = 4 + 4;
+console.log(sum);
+
+let calculate = '4 + 4';
+console.log(calculate);
+
+// antwoord vraag 7b: nee kan niet, want een string is tekst. Dus hij geeft de value weer als tekst ipv dat hij er echt een rekensom van maakt. 
+
+let sumAddition = 3 + 2;
+console.log(sumAddition);
+
+let sumSubstraction = 5 - 1;
+console.log(sumSubstraction);
+
+let sumMultiply = 6 * 3;
+console.log(sumMultiply);
+
+let sumDivision = 10 / 2;
+console.log(sumDivision);
+
+let sumModulus = 5 % 2;
+console.log(sumModulus);
+
+let ageFirst = 48;
+console.log(typeof 48);
+
+let ageSecond = 'fourtyEight';
+console.log(typeof 48);
