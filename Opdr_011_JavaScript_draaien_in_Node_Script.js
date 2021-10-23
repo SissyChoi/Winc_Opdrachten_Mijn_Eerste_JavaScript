a95e8dfc207ed6593d884f1c4bee47b97e2271e9
SissyChoi <90140566+SissyChoi@users.noreply.github.com>
2021-10-23 15:43:41 +0200

    Create Opdr_011_JavaScript_draaien_in_Node_Script.js

---
+++ Opdr_011_JavaScript_draaien_in_Node_Script.js
@@ -0,0 +1,12 @@
+console.log('Hallo Winc Academy studenten');
+
+// Dit is een grote som
+/* let multiply = (1230941 * 1823794);
+console.log(multiply);
+
+let division = (637263 / 54);
+console.log(division); */
+
+// Antwoord vraag 2.4 - Ja, want comments worden niet meegedraait
+
+// Antwoord vraag 2.6 - Nee, want comments worden niet meegedraait
