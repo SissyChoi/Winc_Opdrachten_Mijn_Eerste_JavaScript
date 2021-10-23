a79f7f0c8d7730147bc877e4614b5d292b860e6f
SissyChoi <90140566+SissyChoi@users.noreply.github.com>
2021-10-23 17:33:13 +0200

    Create Opdr_012_JavaScript_If_Else_Statements_Script.js

---
+++ Opdr_012_JavaScript_If_Else_Statements_Script.js
@@ -0,0 +1,25 @@
+const age = 20;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (driverStatus) {
+    console.log("Great job, you are an excelent safe driver!");
+}
+
+else {
+    console.log("Shame on you! Go home, we dont like drunk drivers.")
+}
+
+if (isFemale) {
+    console.log("Hi, beautiful lady!");
+}
+else {
+    console.log("Sorry, its ladies night!");
+}
+
+if (age < 18) {
+    console.log("You are younger than age 18, we will welcome you when we can celebrate your 18th birthday! 🥳");
+}
+else {
+    console.log("You are older than age 18, welcome to our bar!");
+}
